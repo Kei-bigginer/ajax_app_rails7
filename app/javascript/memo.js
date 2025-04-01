905ea08fbdbb0086b95123bb83c5578d96bf4758
Kei-bigginer <new.world.dododo@gmail.com>
2025-04-01 16:58:59 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,19 @@
+function post (){
+// リクエストを送信する処理
+  const form = document.getElementById("form");
+
+
+  form.addEventListener("submit",(e)=> {
+    e.preventDefault();
+
+    const formDate = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST","/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formDate)
+  });
+};
+
+
+
+window.addEventListener(`turbo:load`, post);
